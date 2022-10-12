96d22ff57244851b513c25cbdee92313a81ea124
Matushan Yogaraj <dev@matushan.com>
2022-10-12 22:28:11 +0100

    Raise ValueError if input is less than 1

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
     list = []
+    current_integer = 0
+    while len(list) < number_of_primes:
+        current_integer += 1
+        if is_prime(current_integer):
+            list.append(current_integer)
     return list
+
+def is_prime(integer):
+    if integer <= 1:
+        return False
+    for factor in range(2, int(integer ** 0.5) + 1):
+        if integer % factor == 0:
+            return False
+    else:
+        return True
